80f648e3a8f55bb4f7173ab327317b3d3b4bca47
KamileSeidu <kseidu005@st.ug.edu.gh>
2024-07-20 17:55:58 +0000

    added a home component to hold all my mini components

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import classes from "./Home.module.css";
+import MainNavigation from "./MainNavigation";
+import Hero from "../components/Hero";
+import AboutSection from "../components/AboutSection";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Testimonial from "../components/Testimonial";
+import Contact from "../components/Contact";
+import Footer from "./footer/Footer";
+
+export default function HomePage() {
+  return (
+    <>
+      <section className={classes.container}>
+        <MainNavigation />
+        <Hero />
+        <AboutSection />
+        <Skills />
+        <Projects />
+        <Testimonial />
+        <Contact />
+        <Footer />
+      </section>
+    </>
+  );
+}
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,16 +15,33 @@ export default function Footer() {
         </h1>
       </div>
       <div className={`${classes["social-icons"]}`}>
-        <a href="">
-          <img src={linkedin} alt="" />
+        <a
+          href="https://www.linkedin.com/in/seidu-kamile-19462a2b5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          alt=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedin} />
         </a>
-        <a href="">
+        <a
+          href="https://www.facebook.com/kha.mil.777"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={facebook} alt="" />
         </a>
-        <a href="">
+        <a
+          href="https://x.com/KamileSeidu?t=lRC8PcJKsFGNEMnkFihb2w&s=35"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={twitter} alt="" />
         </a>
-        <a href="">
+        <a
+          href="https://wa.me/qr/E7FJX26GEPAFM1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={whatsapp} alt="" />
         </a>
       </div>
